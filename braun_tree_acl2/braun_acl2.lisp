c697deabaf1178d1c307735ff667eb33b4e0da1d
imlyc <imlyc.mail@gmail.com>
2013-12-19 19:36:14 -0600

    len in stree

--- braun_tree_acl2/braun_acl2.lisp
+++ braun_tree_acl2/braun_acl2.lisp
@@ -2,51 +2,65 @@
 ;;stree is a list, either a nil list or a list with four elements
 ;;nil | size left right data
 
-(defthm not-consp-is-endp
-  (implies (listp l)
-           (implies (not (consp l))
-                    (equal l nil)))
-  :rule-classes (:built-in-clause))#|ACL2s-ToDo-Line|#
-
-
-(defthm cdr-is-list
-  (implies (and (listp l) (consp l))
-           (listp (cdr l)))
-  :hints (("Goal''"
-           :use not-consp-is-endp)))
-
-(defthm streep-lemma
-  (IMPLIES (AND (LISTP TR)
-                (CONSP TR)
-                (NOT (CONSP (CDR TR))))
-           (NOT (CDR TR)))
-  :hints (("Goal"
-           :use not-consp-is-endp)))
-
+;;define tree with size, size tree
 (defun streep (tr)
   (declare (xargs :guard t))
   (if (listp tr)
     (if (endp tr)
       t
-      (if (not (endp (rest tr)))
-        (if (not (endp (rest (rest tr))))
-          (if (not (endp (rest (rest (rest tr)))))
-            (if (endp (rest (rest (rest (rest tr)))))
-              (and (natp (first tr))
-                   (streep (second tr))
-                   (streep (third tr)))
-              nil)
-            nil)
-          nil)
+      (if (equal (len tr) 4)
+        (let ((sz (first tr))
+              (lc (second tr))
+              (rc (third tr)))
+          (if (and (natp sz)
+                   (streep lc)
+                   (streep rc))
+            (let ((lcsz (if (endp lc) 0 (first lc)))
+                  (rcsz (if (endp rc) 0 (first rc))))
+              (equal sz (+ 1 (+ lcsz rcsz))))
+            nil))
         nil))
     nil))
 
+;;selector
+;; size
 (defun stree-size (tr)
   (declare (xargs :guard (streep tr)))
   (if (endp tr)
     0
     (first tr)))
+;; left
+(defun stree-left (tr)
+  (declare (xargs
+            :guard (and (streep tr)
+                        (consp tr))))
+  (second tr))
+;; right
+(defun stree-right (tr)
+  (declare (xargs
+            :guard (and (streep tr)
+                        (consp tr))))
+  (third tr))
 
+;;tree size, iteration
+(defun stree-size-it (tr)
+  (declare (xargs :guard (streep tr)))
+  (if (endp tr)
+    0
+    (+ 1 (+ (stree-size-it (stree-left tr))
+            (stree-size-it (stree-right tr))))))
+
+;;tree size equivalence
+(defthm stree-size-it=stree-size
+  (implies (streep tr)
+           (equal (stree-size-it tr) (stree-size tr))))
+
+;;stree constructor
 (defun streec-leaf () nil)
 
 (defun streec (data lc rc)
+  (declare (xargs :guard (and (streep lc) (streep rc))))
+  (list (+ 1 (+ (stree-size lc) (stree-size rc))) lc rc data))#|ACL2s-ToDo-Line|#
+
+
+;;braun tree
