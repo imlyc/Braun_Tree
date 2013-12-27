d5fa07db2d03bd5be41d380db63b770af1481252
imlyc <imlyc.mail@gmail.com>
2013-12-27 12:10:36 -0600

    remove guard

--- braun_tree_acl2/braun_acl2.lisp
+++ braun_tree_acl2/braun_acl2.lisp
@@ -1,5 +1,7 @@
-(include-book "arithmetic/top-with-meta" :dir :system)
-
+;;arithmetic
+(include-book "arithmetic-5/top" :dir :system)
+;;disable guard verifcation
+;;(set-guard-checking :none)
 ;;braun tree
 ;;stree is a list, either a nil list or a list with four elements
 ;;nil | size left right data
@@ -19,7 +21,7 @@
                    (streep rc))
             (let ((lcsz (if (endp lc) 0 (first lc)))
                   (rcsz (if (endp rc) 0 (first rc))))
-              (equal sz (+ 1 (+ lcsz rcsz))))
+              (equal sz (+ 1 lcsz rcsz)))
             nil))
         nil))
     nil))
@@ -49,13 +51,14 @@
   (declare (xargs :guard (streep tr)))
   (if (endp tr)
     0
-    (+ 1 (+ (stree-size-it (stree-left tr))
-            (stree-size-it (stree-right tr))))))
+    (+ 1  (stree-size-it (stree-left tr))
+            (stree-size-it (stree-right tr)))))
 
 ;;tree size equivalence
 (defthm stree-size-it=stree-size
   (implies (streep tr)
-           (equal (stree-size-it tr) (stree-size tr))))
+           (equal (stree-size-it tr) (stree-size tr)))
+  :rule-classes nil)
 
 ;;stree constructor
 (defun streec-leaf (data)
@@ -63,7 +66,11 @@
 
 (defun streec (data lc rc)
   (declare (xargs :guard (and (streep lc) (streep rc))))
-  (list (+ 1 (+ (stree-size lc) (stree-size rc))) lc rc data))
+  (list (+ 1 (stree-size lc) (stree-size rc)) lc rc data))
+
+(defun streep-null (tr)
+  (declare (xargs :guard (streep tr)))
+  (endp tr))
 
 (defun streep-leaf (tr)
   (declare (xargs :guard (streep tr)))
@@ -72,12 +79,19 @@
     (and (equal (stree-left tr) nil)
          (equal (stree-right tr) nil))))
 
+
+
+
+;;(in-theory (disable streep streep-leaf streep-null 
+  ;;                  stree-left stree-right stree-size))
+
+
+
 ;;braun tree
 (defun brtreep (brtr)
-;;  (declare (xargs :guard (streep brtr)))
   (declare (xargs :guard t))
   (if (streep brtr)
-    (if (endp brtr)
+    (if (streep-null brtr)
       t
       (let ((lc (stree-left brtr))
             (rc (stree-right brtr)))
@@ -97,11 +111,7 @@
                               (natp num))))
   (let ((sz (stree-size brtr)))
     (or (equal sz num)
-        (equal sz (+ num 1)))))#|ACL2s-ToDo-Line|#
-
-
-;;number theory
-;;(in-theory (disable evenp oddp))
+        (equal sz (+ num 1)))))
 
 ;;braun tree diff
 (defun brtree-diff (brtr num)
@@ -118,11 +128,31 @@
     (brtree-diff (stree-right brtr) (/ (- num 2) 2)))))
 
 ;;braun size
+#|
+     (IMPLIES (AND (BRTREEP BRTR) (NOT (ENDP BRTR)))
+              (LET ((M (BRTREE-SIZE (STREE-RIGHT BRTR)))
+                    (S (STREE-LEFT BRTR)))
+                   (AND (ACL2-NUMBERP M)
+                        (NATP M)
+                        (STREE-SIZE-NUMP S M)
+                        (BRTREEP S)
+                        (ACL2-NUMBERP (* 2 M))
+                        (ACL2-NUMBERP (BRTREE-DIFF S M))
+                        (ACL2-NUMBERP (+ (* 2 M) (BRTREE-DIFF S M))))))
+|#
+;;(in-theory (disable streep));; stree-left stree-right));; stree-size))
+
+
 (defun brtree-size (brtr)
-  (declare (xargs :guard (brtreep brtr)))
+;;  (declare (xargs :guard (brtreep brtr)))
   (if (endp brtr)
     0
     (let ((m (brtree-size (stree-right brtr)))
-          (s (stree-left brtr)))
-          (braun-diff s m))))
-  
+         (s (stree-left brtr)))
+          (+ 1 (* 2 m) (brtree-diff s m)))))
+(in-theory (disable streep stree-left stree-right stree-size))#|ACL2s-ToDo-Line|#
+
+(defthm brtee-size-correct
+  (implies (brtreep brtr)
+           (equal (brtree-size brtr) (stree-size-it brtr)))
+  :rule-classes nil)
